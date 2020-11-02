3af6e1d65567b459f04d79b525eb5b835bf3aae9
Saphogames <56717091+Saphogames@users.noreply.github.com>
2020-11-02 17:21:30 +0100

    Gardons les nom simple

---
+++ Jormun/app/src/main/java/com/example/jormun_propre/Simulation_Import_DB.java
@@ -0,0 +1,9 @@
+package com.example.jormun_propre;
+
+public class Simulation_Import_DB
+{
+    public void start_Simulation()
+    {
+
+    }
+}
--- Jormun_Propre/app/src/main/java/com/example/jormun_propre/Simulation_Import_DB.java
+++
@@ -1,9 +0,0 @@
-package com.example.jormun_propre;
-
-public class Simulation_Import_DB
-{
-    public void start_Simulation()
-    {
-
-    }
-}
